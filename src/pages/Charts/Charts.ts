c3e9fd869c128da61fcbfbfd94302993d60ce00c
Chris McKinnon <cpmckinnonii@gmail.com>
2018-02-08 19:20:08 -0600

    my changes

--- src/pages/Charts/Charts.ts
+++ src/pages/Charts/Charts.ts
@@ -1,37 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Chart } from 'chart.js'
 
 @Component({
-  selector: 'Charts',
+  selector: 'charts-page',
   templateUrl: 'Charts.html'
 })
-export class ChartsPage {
+export class ChartsPage implements OnInit {
+  ngOnInit(): void {
+    var canvas =  <HTMLCanvasElement> document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    console.log('not here')
+    var myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
+        datasets: [{
+          label: 'apples',
+          data: [12, 19, 3, 17, 6, 3, 7],
+          backgroundColor: "rgba(153,255,51,0.6)"
+        }, {
+          label: 'oranges',
+          data: [2, 29, 5, 5, 2, 3, 10],
+          backgroundColor: "rgba(255,153,0,0.6)"
+        }]
+      }
+    });
+    myChart.update();
+    ctx.font ="30px Verdana"
+    ctx.fillText("Hello World",100,100)
+    //console.log(myChart);
+  }
+
   constructor() {
 
   }
 }
 function init(){
-  var canvas =  <HTMLCanvasElement> document.getElementById("myCanvas");
-  var ctx = canvas.getContext("2d");
-  console.log('not here')
-  var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
-      datasets: [{
-        label: 'apples',
-        data: [12, 19, 3, 17, 6, 3, 7],
-        backgroundColor: "rgba(153,255,51,0.6)"
-      }, {
-        label: 'oranges',
-        data: [2, 29, 5, 5, 2, 3, 10],
-        backgroundColor: "rgba(255,153,0,0.6)"
-      }]
-    }
-  });
-  myChart.update();
-  ctx.font ="30px Verdana"
-  ctx.fillText("Hello World",100,100)
-  //console.log(myChart);
+ 
 }
 document.addEventListener("DOMContentLoaded", init, false);
